1b95a147ebdb9a5c87b2501ba7f9bae3db756d62
Vassily-Petrousevitch <vasspetrousevitch@gmail.com>
2024-02-23 18:45:57 -0800

    better movement, turning, brakes
    semi-set top speed, good braking, decent-ish turning based on linear damping

--- entities/player.gd
+++ entities/player.gd
@@ -4,21 +4,31 @@ extends Node2D
 const rotate_speed = 0.07;
 
 # Force constants
-const thrust = Vector2(2500, 0);
-const brake_scalar = 300;	# Almost like reverse thrusters
+const thrust = Vector2(5000, 0);
+const thrust_damp_scalar = 0.001;
+const min_brake = 1;	
+const max_brake = 1000;
 
 func _integrate_forces(state):
-	# Rotation
+
 	var rotation_direction = findNewRotation();
 	self.rotation += rotation_direction * rotate_speed;
 	
-	# TODO: implement max speed (for thrusters)
 	if Input.is_action_pressed("accelerate"):
+		# Damps based on current velocity
+		self.linear_damp = generateLinDamp();
 		state.apply_force(thrust.rotated(self.rotation));
 	else:
+		self.linear_damp = 0;	# or something else
 		state.apply_force(Vector2());
 	
-	# TODO: add braking using lin damp
+	if Input.is_action_pressed("brake"):
+		self.linear_damp = clamp(generateLinDamp(), min_brake, max_brake);
+
+# returns c * thrust_damp_scalar, where c^2 = velocity.x^2 + velocity.y^2
+func generateLinDamp():
+	return pow(pow(self.linear_velocity.x, 2) 
+				+ pow(self.linear_velocity.y, 2), 0.5) * thrust_damp_scalar;
 
 func findNewRotation():
 	var rotate_direction = 0;
