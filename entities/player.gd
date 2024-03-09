d9545ce1ec418e88e0141e3261655b21ea0a4b09
Vassily Petrousevitch <vasspetrousevitch@gmail.com>
2024-03-09 14:39:43 -0800

    genuinely think this is the best movement I've done

--- entities/player.gd
+++ entities/player.gd
@@ -5,23 +5,16 @@ const rotate_speed = 0.07;
 
 # Velocity & accel
 var velocity = Vector2.ZERO
-var max_velocity = Vector2(1000, 1000)
-var accel_h = 10
-var accel_v = 10
+var max_velocity = Vector2(1000, 0)
+var accel = 50
 
 func _process(delta):
 	self.rotation = fmod(self.rotation + findNewRotation() * rotate_speed, 2 * PI)
 	
 	if Input.is_action_pressed("accelerate"):
-		var new_speed = Vector2(accel_h * cos(self.rotation), accel_v * sin(self.rotation))
-		
-		if (abs(new_speed.x + velocity.x) < max_velocity.x):
-			velocity.x += new_speed.x
-			
-		if (abs(new_speed.y + velocity.y) < max_velocity.y):
-			velocity.y += new_speed.y
-		
-	print(velocity)
+		velocity = velocity.move_toward(max_velocity.rotated(self.rotation), accel)
+		print(velocity)
+
 	position += velocity * delta
 
 func findNewRotation():
