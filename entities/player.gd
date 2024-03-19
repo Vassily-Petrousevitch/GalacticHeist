e9a1aec194c1f4ea40ef6375c61e2d935a83714c
Vassily-Petrousevitch <vasspetrousevitch@gmail.com>
2024-03-18 18:09:56 -0700

    illegally pushing changes direct to master

--- entities/player.gd
+++ entities/player.gd
@@ -1,13 +1,13 @@
 extends Node2D
 
 # General constants
-const rotate_speed = 0.07;
+const rotate_speed = 0.05;
 
 # Velocity & accel
 var velocity = Vector2.ZERO
 var max_velocity = Vector2(1000, 0)
 var accel = 40
-var brake_accel = 100
+var brake_accel = 0.1
 
 func _process(delta):
 	self.rotation = fmod(self.rotation + findNewRotation() * rotate_speed, 2 * PI)
@@ -15,7 +15,7 @@ func _process(delta):
 	if Input.is_action_pressed("accelerate"):
 		velocity = velocity.move_toward(max_velocity.rotated(self.rotation), accel)
 	if Input.is_action_pressed("brake"):
-		velocity = velocity.lerp(Vector2.ZERO, 0.1)
+		velocity = velocity.lerp(Vector2.ZERO, brake_accel)
 
 	position += velocity * delta
 
