3cac1aa1dce28d48254e6a0b4b366795e61f32aa
Vassily-Petrousevitch <vasspetrousevitch@gmail.com>
2024-03-31 23:30:40 -0700

    goofy 'bullet' behavior

--- entities/player.gd
+++ entities/player.gd
@@ -10,6 +10,11 @@ const brake_accel = 0.15
 const ship_bounceback = 0.25
 var stunned = false
 
+# Bullet parameters
+const bullet_scene = preload("res://entities/bullet.tscn")
+const bullet_timeout = 0.5		# seconds
+
+
 func _physics_process(delta):
 	if not stunned:
 		get_input()
@@ -23,10 +28,16 @@ func _physics_process(delta):
 
 func get_stunned():
 	stunned = true
-	self.modulate = Color("4d4d4d")
+	self.modulate = Color("4d4d4d")		# overlay with a gray filter
 	await get_tree().create_timer(0.5).timeout # waits briefly
 	stunned = false
 	self.modulate = Color("fff")
+
+func shoot_bullet():
+	var bullet = bullet_scene.instantiate()
+	bullet.velocity = Vector2(10, 10)
+	bullet.position = get_global_position() + Vector2(15, 15)
+	get_parent().add_child(bullet)
 
 func get_input():
 	setRotation()
@@ -36,6 +47,9 @@ func get_input():
 		velocity = velocity.move_toward(max_velocity.rotated(self.rotation), accel)
 	if Input.is_action_pressed("brake"):
 		velocity = velocity.lerp(Vector2.ZERO, brake_accel)
+		
+	if Input.is_action_just_pressed("shoot"):
+		shoot_bullet()
 
 func setRotation():
 	var rotate_direction = 0;
